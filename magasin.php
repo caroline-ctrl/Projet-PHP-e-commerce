99c41ec64fbea0c9fe863fff454011a4e499acf4
ajoanaverde <ajoanaverde@gmail.com>
2019-12-17 16:19:43 +0100

    more fusions

--- magasin.php
+++ magasin.php
@@ -1,52 +1,52 @@
+<?php
+$server = 'localhost';
+$login = 'root';
+$pass = '';
 
-    <?php
-    $server = 'localhost';
-    $login = 'root';
-    $pass = '';
-    
-    try {
-        $connection = new PDO("mysql:host=$server;dbname=present", $login, $pass);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (Exception $e) {
-        echo $e->getMessage();
-        die();
-    }
-     include('vue/header.php'); ?>
-
-    
-    <?php $recup = $connection->query('SELECT images, descriptions, prix FROM produit ORDER BY id'); ?>
-
-        
-            <div class="col-9">
-                <div class="row test">
+try {
+    $connection = new PDO("mysql:host=$server;dbname=present", $login, $pass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
+include('vue/header.php'); ?>
 
 
-                    
-                    <?php 
-                    while ($donnees = $recup->fetch()){ ?>
-                        <div class="col-4">
-                        <br><img id="img1" src="<?= $donnees['images']?>" alt="affiche image"><br>
-                        <?php echo $donnees['descriptions'] . '<br>';
-                        echo $donnees['prix'] . ' € <hr><br><br>'; ?>
-                        </div>
-                    <?php } ?>
-                
-                    
-                
-                </div>
-                    
+<?php $recup = $connection->query('SELECT images, descriptions, prix FROM produit ORDER BY id'); ?>
 
-                
-                
-                
-                
-            
+
+<div class="col-9">
+    <div class="row test">
+
+
+
+        <?php
+        while ($donnees = $recup->fetch()) { ?>
+            <div class="col-4">
+                <br><img id="img1" src="<?= $donnees['images'] ?>" alt="affiche image"><br>
+                <?php echo $donnees['descriptions'] . '<br>';
+                                        echo $donnees['prix'] . ' € <hr><br><br>'; ?>
             </div>
+        <?php } ?>
 
-        
+
+
     </div>
-    
 
-    <?php include('vue/footer.php'); ?>
+
+
+
+
+
+
+</div>
+
+
+</div>
+
+
+<?php include('vue/footer.php'); ?>
 </body>
+
 </html>
